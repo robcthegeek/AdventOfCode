d5fbc4b40016f963a4b834d091ac5593fa69c330
Rob Cooper <robc.the.geek@gmail.com>
2019-12-01 11:22:09 +0000

    Day 1.1 Done

--- AdventOfCode.Tests/Day1Tests.cs
+++ AdventOfCode.Tests/Day1Tests.cs
@@ -1,14 +1,44 @@
 using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
 using Xunit;
 
 namespace AdventOfCode.Tests
 {
     public class Day1Tests
     {
-        [Fact]
-        public void Test1()
+        [Theory]
+        [InlineData(12, 2)]
+        [InlineData(14, 2)]
+        [InlineData(1969, 654)]
+        [InlineData(100756, 33583)]
+        public void fuelrequired_returns_expected_for_given_mass(int mass, int expected)
         {
-            Assert.True(Day1.IsThere);
+            var actual = Day1.FuelRequired(mass);
+
+            Assert.Equal(expected, actual);
         }
+
+        [Fact]
+        public void can_solve_part_one()
+        {
+            var result = Input.Lines(1, 1)
+                .Select(x => Day1.FuelRequired(int.Parse(x)))
+                .Sum();
+            
+            Console.WriteLine(result);
+
+            Assert.True(result > 0);
+        }
+    }
+
+    public static class Input
+    {
+        public static string Day(int dayNumber, int partNumber) =>
+            File.ReadAllText($"Input\\Day{dayNumber}.{partNumber}.txt");
+
+        public static IEnumerable<string> Lines(int dayNumber, int partNumber) =>
+            Day(dayNumber, partNumber).Split(Environment.NewLine);
     }
 }
--- AdventOfCode/Day1.cs
+++ AdventOfCode/Day1.cs
@@ -2,8 +2,9 @@
 
 namespace AdventOfCode
 {
+    // Day 1: The Tyranny of the Rocket Equation
     public class Day1
     {
-        public static bool IsThere => true;
+        public static int FuelRequired(int mass) => Convert.ToInt32(Math.Floor(((double)mass / 3) - 2));
     }
 }
