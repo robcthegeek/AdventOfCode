aa463b20b0209e050e59bf0d5b918b8d34b085e4
Rob Cooper <robc.the.geek@gmail.com>
2019-12-01 11:01:44 +0000

    HERE WE GO!

---
+++ AdventOfCode.Tests/Day1Tests.cs
@@ -0,0 +1,14 @@
+using System;
+using Xunit;
+
+namespace AdventOfCode.Tests
+{
+    public class Day1Tests
+    {
+        [Fact]
+        public void Test1()
+        {
+            Assert.True(Day1.IsThere);
+        }
+    }
+}
---
+++ AdventOfCode/Day1.cs
@@ -0,0 +1,9 @@
+﻿using System;
+
+namespace AdventOfCode
+{
+    public class Day1
+    {
+        public static bool IsThere => true;
+    }
+}
