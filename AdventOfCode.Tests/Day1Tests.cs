8f60f8585b57d17299b16434388e091990f9d69b
Rob Cooper <robc.the.geek@gmail.com>
2019-12-01 11:37:42 +0000

    Day 1, Part 2 Completed ⭐⭐

--- AdventOfCode.Tests/Day1Tests.cs
+++ AdventOfCode.Tests/Day1Tests.cs
@@ -23,10 +23,33 @@ namespace AdventOfCode.Tests
         [Fact]
         public void can_solve_part_one()
         {
-            var result = Input.Lines(1, 1)
-                .Select(x => Day1.FuelRequired(int.Parse(x)))
+            var result = Input
+                .Ints(1, 1)
+                .Select(Day1.FuelRequired)
                 .Sum();
             
+            Console.WriteLine(result);
+
+            Assert.True(result > 0);
+        }
+
+        [Theory]
+        [InlineData(14, 2)]
+        [InlineData(1969, 966)]
+        public void totalfuelrequired_returns_expected_for_given_mass(int mass, int expected)
+        {
+            var actual = Day1.TotalFuelRequired(mass);
+
+            Assert.Equal(expected, actual);
+        }
+
+        [Fact]
+        public void can_solve_part_two()
+        {
+            var result = Input.Ints(1, 1)
+                .Select(Day1.TotalFuelRequired)
+                .Sum();
+
             Console.WriteLine(result);
 
             Assert.True(result > 0);
@@ -40,5 +63,8 @@ namespace AdventOfCode.Tests
 
         public static IEnumerable<string> Lines(int dayNumber, int partNumber) =>
             Day(dayNumber, partNumber).Split(Environment.NewLine);
+
+        public static IEnumerable<int> Ints(int dayNumber, int partNumber) =>
+            Day(dayNumber, partNumber).Split(Environment.NewLine).Select(int.Parse);
     }
 }
--- AdventOfCode/Day1.cs
+++ AdventOfCode/Day1.cs
@@ -6,5 +6,18 @@ namespace AdventOfCode
     public class Day1
     {
         public static int FuelRequired(int mass) => Convert.ToInt32(Math.Floor(((double)mass / 3) - 2));
+        public static int TotalFuelRequired(int mass)
+        {
+            int result = 0;
+            int additional;
+
+            while ((additional = FuelRequired(mass)) > 0)
+            {
+                result += additional;
+                mass = additional;
+            }
+
+            return result;
+        }
     }
 }
