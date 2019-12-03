838992d2bc98fcaa567f87913099d4ffc7f597e4
Rob Cooper <rob.cooper@thetrainline.com>
2019-12-03 11:05:05 +0000

    Day 3, Part 2 ⭐⭐
    Pretty hacky, but does the job 😊

--- AdventOfCode.Tests/Day3Tests.cs
+++ AdventOfCode.Tests/Day3Tests.cs
@@ -44,5 +44,42 @@ namespace AdventOfCode.Tests
             Console.WriteLine(answer);
             Assert.Equal(2050, answer);
         }
+
+        [Fact]
+        public void can_get_quickest_intersection()
+        {
+            var grid = new Grid();
+            grid.Lay("R8,U5,L5,D3");
+            grid.Lay("U7,R6,D4,L4");
+            Assert.Equal(30, grid.LengthToQuickestIntersection);
+        }
+
+        [Fact]
+        public void part_2_sample_1()
+        {
+            var grid = new Grid();
+            grid.Lay("R75,D30,R83,U83,L12,D49,R71,U7,L72");
+            grid.Lay("U62,R66,U55,R34,D71,R55,D58,R83");
+            Assert.Equal(610, grid.LengthToQuickestIntersection);
+        }
+
+        [Fact]
+        public void part_2_sample_2()
+        {
+            var grid = new Grid();
+            grid.Lay("R98,U47,R26,D63,R33,U87,L62,D20,R33,U53,R51");
+            grid.Lay("U98,R91,D20,R16,D67,R40,U7,R15,U6,R7");
+            Assert.Equal(410, grid.LengthToQuickestIntersection);
+        }
+
+        [Fact]
+        public void can_solve_part_2()
+        {
+            var grid = new Grid();
+            Input.Lines(3).ToList().ForEach(grid.Lay);
+            var answer = grid.LengthToQuickestIntersection;
+            Console.WriteLine(answer);
+            Assert.Equal(21666, answer);
+        }
     }
 }
--- AdventOfCode/Day3.cs
+++ AdventOfCode/Day3.cs
@@ -11,41 +11,47 @@ namespace AdventOfCode
     {
         private static readonly Regex Parser = new Regex("(?<dir>[UDLR])(?<len>\\d+)", RegexOptions.Compiled);
 
-        private readonly HashSet<Coord> _wired = new HashSet<Coord>
-        {
-            new Coord(0, 0)
-        };
+        private readonly Dictionary<Coord, int> _wired = new Dictionary<Coord, int>();
 
-        private readonly HashSet<Coord> _crossed = new HashSet<Coord>();
+        private readonly Dictionary<Coord, int> _crossed = new Dictionary<Coord, int>();
 
-        public int DistanceToClosestIntersection =>
-            _crossed
-                .Select(x => Manhattan.Distance(new Coord(0, 0), x))
+        public int DistanceToClosestIntersection => _crossed
+                .Select(x => Manhattan.Distance(new Coord(0, 0), x.Key))
                 .OrderBy(x => x)
                 .First();
+
+        public int LengthToQuickestIntersection { get; private set; } = -1;
 
         public void Lay(string wire)
         {
             var path = wire.Split(',');
-            var loc = new Coord(0,0);
+            var loc = new Coord(0, 0);
+            int distance = 0;
 
             var current = new HashSet<Coord>();
-            void Track(Coord coord)
+            void Track(Coord coord, int distance)
             {
-                if (!current.Contains(coord) && _wired.Contains(loc))
-                    _crossed.Add(loc);
+                if (!current.Contains(coord) && _wired.ContainsKey(loc))
+                {
+                    var otherWireDistance = _wired[loc];
+                    int totalDistance = distance + otherWireDistance;
+                    _crossed[loc] = totalDistance;
 
-                _wired.Add(coord);
+                    if (LengthToQuickestIntersection == -1 || totalDistance < LengthToQuickestIntersection)
+                        LengthToQuickestIntersection = totalDistance;
+                }
+                _wired[coord] = distance;
                 current.Add(coord);
             }
-            
+
             foreach (var leg in path)
             {
                 var instr = Parser.Match(leg);
                 for (var i = 0; i < int.Parse(instr.Groups["len"].Value); i++)
                 {
+                    distance++;
                     loc = loc.Shift(instr.Groups["dir"].Value);
-                    Track(loc);
+                    Track(loc, distance);
                 }
             }
         }
@@ -61,10 +67,10 @@ namespace AdventOfCode
         {
             switch (dir)
             {
-                case "U": return new Coord(X, Y-1);
-                case "D": return new Coord(X, Y+1);
-                case "L": return new Coord(X-1, Y);
-                case "R": return new Coord(X+1, Y);
+                case "U": return new Coord(X, Y - 1);
+                case "D": return new Coord(X, Y + 1);
+                case "L": return new Coord(X - 1, Y);
+                case "R": return new Coord(X + 1, Y);
                 default: throw new ArgumentException("Unexpected Direction");
             }
         }
@@ -74,6 +80,8 @@ namespace AdventOfCode
             X = x;
             Y = y;
         }
+
+        public override string ToString() => $"{X},{Y}";
     }
 
     internal static class Manhattan
