0d00412d2ab686888f0ccd559e09dd2075286803
Rob Cooper <robc.the.geek@gmail.com>
2019-12-03 06:14:32 +0000

    Day 3, Part 1 ⭐

---
+++ AdventOfCode.Tests/Day3Tests.cs
@@ -0,0 +1,48 @@
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using Xunit;
+
+namespace AdventOfCode.Tests
+{
+    public class Day3Tests
+    {
+        [Fact]
+        public void can_solve_first_sample()
+        {
+            var grid = new Grid();
+            grid.Lay("R8,U5,L5,D3");
+            grid.Lay("U7,R6,D4,L4");
+            Assert.Equal(6, grid.DistanceToClosestIntersection);
+        }
+
+        [Fact]
+        public void can_solve_second_sample()
+        {
+            var grid = new Grid();
+            grid.Lay("R75,D30,R83,U83,L12,D49,R71,U7,L72");
+            grid.Lay("U62,R66,U55,R34,D71,R55,D58,R83");
+            Assert.Equal(159, grid.DistanceToClosestIntersection);
+        }
+
+        [Fact]
+        public void can_solve_third_sample()
+        {
+            var grid = new Grid();
+            grid.Lay("R98,U47,R26,D63,R33,U87,L62,D20,R33,U53,R51");
+            grid.Lay("U98,R91,D20,R16,D67,R40,U7,R15,U6,R7");
+            Assert.Equal(135, grid.DistanceToClosestIntersection);
+        }
+
+        [Fact]
+        public void can_solve_puzzle()
+        {
+            var grid = new Grid();
+            Input.Lines(3).ToList().ForEach(grid.Lay);
+            var answer = grid.DistanceToClosestIntersection;
+            Console.WriteLine(answer);
+            Assert.Equal(2050, answer);
+        }
+    }
+}
---
+++ AdventOfCode/Day3.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text.RegularExpressions;
+
+namespace AdventOfCode
+{
+    // Day 3: Crossed Wires
+    public class Grid
+    {
+        private static readonly Regex Parser = new Regex("(?<dir>[UDLR])(?<len>\\d+)", RegexOptions.Compiled);
+
+        private readonly HashSet<Coord> _wired = new HashSet<Coord>
+        {
+            new Coord(0, 0)
+        };
+
+        private readonly HashSet<Coord> _crossed = new HashSet<Coord>();
+
+        public int DistanceToClosestIntersection =>
+            _crossed
+                .Select(x => Manhattan.Distance(new Coord(0, 0), x))
+                .OrderBy(x => x)
+                .First();
+
+        public void Lay(string wire)
+        {
+            var path = wire.Split(',');
+            var loc = new Coord(0,0);
+
+            var current = new HashSet<Coord>();
+            void Track(Coord coord)
+            {
+                if (!current.Contains(coord) && _wired.Contains(loc))
+                    _crossed.Add(loc);
+
+                _wired.Add(coord);
+                current.Add(coord);
+            }
+            
+            foreach (var leg in path)
+            {
+                var instr = Parser.Match(leg);
+                for (var i = 0; i < int.Parse(instr.Groups["len"].Value); i++)
+                {
+                    loc = loc.Shift(instr.Groups["dir"].Value);
+                    Track(loc);
+                }
+            }
+        }
+    }
+
+    [DebuggerDisplay("{X},{Y}")]
+    internal struct Coord
+    {
+        public int X { get; set; }
+        public int Y { get; set; }
+
+        public Coord Shift(string dir)
+        {
+            switch (dir)
+            {
+                case "U": return new Coord(X, Y-1);
+                case "D": return new Coord(X, Y+1);
+                case "L": return new Coord(X-1, Y);
+                case "R": return new Coord(X+1, Y);
+                default: throw new ArgumentException("Unexpected Direction");
+            }
+        }
+
+        public Coord(int x, int y)
+        {
+            X = x;
+            Y = y;
+        }
+    }
+
+    internal static class Manhattan
+    {
+        public static int Distance(Coord a, Coord b) => Math.Abs(a.X - b.X) + Math.Abs(a.Y - b.Y);
+    }
+}
