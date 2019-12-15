01a8d8c2c71559208138c84a28f74ebc65eeddbb
Rob Cooper <robc.the.geek@gmail.com>
2019-12-15 18:28:06 +0000

    Day 8, Part 2 ⭐⭐

--- AdventOfCode.Tests/Day8Tests.cs
+++ AdventOfCode.Tests/Day8Tests.cs
@@ -1,4 +1,4 @@
-using System;
+﻿using System;
 using System.Collections.Generic;
 using System.Linq;
 using Xunit;
@@ -20,11 +20,40 @@ namespace AdventOfCode.Tests
         {
             var sif = new Day8.SifImage(25, 6, Input.Day(8));
             var layer = sif.LayerWithFewest(0);
-            var num1 = layer.Count(c => c == 1.DigitToChar());
-            var num2 = layer.Count(c => c == 2.DigitToChar());
+            var num1 = layer.Count(c => c == 1.ToChar());
+            var num2 = layer.Count(c => c == 2.ToChar());
             var ans = num1 * num2;
             Console.WriteLine($"Count '1' ({num1}) * Count '2' ({num2}) = {ans}");
             Assert.Equal(1905, ans);
+        }
+
+        [Fact]
+        public void can_solve_part_2()
+        {
+            var image = new Day8.SifImage(25, 6, Input.Day(8)).Render();
+            var expected =
+                "█░░███░░██░██░█░░░██░░░░█" + "\r\n" +
+                "░██░█░██░█░█░██░██░████░█" + "\r\n" +
+                "░██░█░████░░███░██░███░██" + "\r\n" +
+                "░░░░█░████░█░██░░░███░███" + "\r\n" +
+                "░██░█░██░█░█░██░████░████" + "\r\n" +
+                "░██░██░░██░██░█░████░░░░█";
+
+            Console.WriteLine(image);
+            Assert.Equal(expected, image);
+        }
+
+        [Theory]
+        [InlineData(1, 1, "0", "█")]                       // Black
+        [InlineData(1, 1, "1", "░")]                       // White
+        [InlineData(2, 1, "0122", "█░")]                   // Transparent 2nd Layer
+        [InlineData(2, 2, "0222112222120000", "█░\r\n░█")] // Sample Image
+        public void can_render_sif_stream(int width, int height, string input, string expected)
+        {
+            var sif = new Day8.SifImage(width, height, input);
+            var image = sif.Render();
+            Console.WriteLine(image);
+            Assert.Equal(expected, image);
         }
     }
 }
--- AdventOfCode/Day8.cs
+++ AdventOfCode/Day8.cs
@@ -10,10 +10,19 @@ namespace AdventOfCode
     {
         public class SifImage
         {
+            private readonly char[,] _image;
+
+            private static class Color
+            {
+                internal const int Black = 0;
+                internal const int White = 1;
+                internal const int Transparent = 2;
+            }
+
             public List<string> Layers { get; } = new List<string>();
 
-            public string LayerWithFewest(int digit) => LayerWithFewest(digit.DigitToChar());
-            
+            public string LayerWithFewest(int digit) => LayerWithFewest(digit.ToChar());
+
             private string LayerWithFewest(char @char) =>
                 Layers
                     .OrderBy(l => l.Count(c => c == @char))
@@ -21,28 +30,56 @@ namespace AdventOfCode
 
             public SifImage(int width, int height, string digits)
             {
+                _image = new char[width, height];
+
                 var chars = new Queue<char>(digits);
                 var layer = new StringBuilder();
-
+                var z = 0;
                 while (chars.Count > 0)
                 {
-                    for (int y = 0; y < height; y++)
+                    for (var y = 0; y < height; y++)
                     {
-                        for (int x = 0; x < width; x++)
+                        for (var x = 0; x < width; x++)
                         {
-                            layer.Append(chars.Dequeue());
+                            if (z == 0) _image[x, y] = Color.Transparent.ToChar();
+
+                            var c = chars.Dequeue();
+                            layer.Append(c);
+
+                            if (_image[x, y] == Color.Transparent.ToChar() && (c == '0' || c == '1'))
+                            {
+                                _image[x, y] = c.ToDigit() == Color.Black ? '█' : '░';
+                            }
                         }
                     }
 
+                    z++;
                     Layers.Add(layer.ToString());
                     layer.Clear();
                 }
+            }
+
+            public string Render()
+            {
+                var sb = new StringBuilder();
+                for (var y = 0; y < _image.GetLength(1); y++)
+                {
+                    for (var x = 0; x < _image.GetLength(0); x++)
+                    {
+                        sb.Append(_image[x, y]);
+                    }
+
+                    sb.AppendLine();
+                }
+
+                return sb.ToString().Trim();
             }
         }
     }
 
     public static class Extensions
     {
-        public static char DigitToChar(this int digit) => Convert.ToChar(digit + 48);
+        public static char ToChar(this int digit) => Convert.ToChar(digit + 48);
+        public static int ToDigit(this char @char) => Convert.ToInt32(@char - 48);
     }
 }
