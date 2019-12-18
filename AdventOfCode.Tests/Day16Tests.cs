aaea567a5ccfb2bc0099c65f0d3d91dbe8d6dbd7
Rob Cooper <rob.cooper@thetrainline.com>
2019-12-18 12:55:30 +0000

    Prep Day 16

---
+++ AdventOfCode.Tests/Day16Tests.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using Xunit;
+
+namespace AdventOfCode.Tests
+{
+    public class Day16Tests
+    {
+
+    }
+}
---
+++ AdventOfCode/Day16.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace AdventOfCode
+{
+    // Day 16: Flawed Frequency Transmission
+    public class Day16
+    {
+
+    }
+}
