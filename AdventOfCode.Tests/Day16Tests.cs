40a6d9b519b3cba336c55fe12c511faa30c0c6ce
Rob Cooper <rob.cooper@thetrainline.com>
2019-12-18 16:34:04 +0000

    WIP

--- AdventOfCode.Tests/Day16Tests.cs
+++ AdventOfCode.Tests/Day16Tests.cs
@@ -7,6 +7,17 @@ namespace AdventOfCode.Tests
 {
     public class Day16Tests
     {
+        [Theory]
+        [InlineData("12345678", "48226158", "34040438", "03415518", "01029498")]
+        public void sample_is_working(string input, params string[] phases)
+        {
+            var fft = new Day16.FFT(input);
 
+            for (int i = 0; i < phases.Length; i++)
+            {
+                var result = fft.Phase();
+                Assert.Equal(phases[i], result);
+            }
+        }
     }
 }
--- AdventOfCode/Day16.cs
+++ AdventOfCode/Day16.cs
@@ -8,6 +8,27 @@ namespace AdventOfCode
     // Day 16: Flawed Frequency Transmission
     public class Day16
     {
+        public class FFT
+        {
+            private int[] _current;
+            private int[] _next;
 
+            public FFT(string input)
+            {
+                var ints = input.Select(x => x - 48); //.ToArray();
+
+                _next = new int[ints.Count()];
+
+                for (int i = 0; i < ints.Count(); i++)
+                {
+
+                }
+            }
+
+            public string Phase()
+            {
+                return string.Empty;
+            }
+        }
     }
 }
